4d926d4213d4b226c93c0874eb11e43d8a0ee66f
Ethan Swan <eswan18@rocketmail.com>
2019-02-21 18:22:51 -0500

    Add to framework for API.

--- nba_api/api/api.py
+++ nba_api/api/api.py
@@ -3,5 +3,22 @@ class API():
         self.year = year
         self.team = team
 
-    def get_playbyplay(self, year=None, team=None, season_segment=None):
-        raise NotImplementedError
+    def get_playbyplay(self, team=None, year=None, season_segment=None):
+        team_id = self._resolve_team_ref(team)
+        season_id = self._fmt_year_as_season(year)
+
+    def _resolve_team_ref(self, team_str):
+        return NotImplemented
+
+    def _fmt_year_as_season_id(self, year):
+        year = str(year)
+        return year + str(int(year + 1))[:2]
+
+    def get_game_info(self):
+        return NotImplemented
+
+    def _get_event_msg_type_desc(self, event_msg_type):
+        return NotImplemented
+
+    def get_box_scores(self):
+        return NotImplemented
